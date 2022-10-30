8910d00f0bac763a073fefd86ce61f2ebf2c125b
jronnomo <ggronnii@gmail.com>
2022-10-30 18:37:49 -0400

    more practice

--- DomPractice/HideElementPractice.js
+++ DomPractice/HideElementPractice.js
@@ -5,3 +5,21 @@ document.querySelector('#siteSub').addEventListener('click', hideThis)
 function hideThis(){
     document.querySelector('#firstHeading').style.display = 'none'
 }
+
+
+// 1) Check if a class is included from an array of html elements containing a separate class
+// 2) On Click, perform a function that hides the element based on if the class is included
+// 3) If the element clicked doesn't include that separate class, alert "wrong!"
+
+Array.from(document.querySelectorAll('.firstClass')).forEach(element => element.addEventListener('click', doThis))
+
+function doThis(click){
+    if(click.target.classList.contains('classInQuestion')){
+        document.querySelector('#elementContainingSepClass').classList.toggle('hidden')
+    }
+    else{
+        alert('wrong!')
+    }
+}
+
+
