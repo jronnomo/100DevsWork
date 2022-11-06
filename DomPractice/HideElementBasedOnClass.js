8dbb3bcbf863b2f3a2cd45a46e674fae795331ce
jronnomo <ggronnii@gmail.com>
2022-11-06 10:37:33 -0500

    added some DOM manipulation notes

---
+++ DomPractice/HideElementBasedOnClass.js
@@ -0,0 +1,16 @@
+Array.from(document.querySelectorAll('.class1')).forEach(element => element.addEventListener('click', hideThis));
+
+function hideThis(click){
+    if(click.target.classList.contains('.class2')){
+        document.querySelector('#elementContainingSepClass').classList.toggle('hidden')
+    }
+    else{
+        alert('wrong!')
+    }
+}
+
+//struggle notes
+
+// click.target.classList.contains
+
+// even when click is on the element in question we still have to querySelector the element ID and add the hidden class to it
