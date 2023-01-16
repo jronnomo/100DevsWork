18724f16b93e023017b0755ceb83379ab6a64fe0
jronnomo <ggronnii@gmail.com>
2023-01-16 13:49:40 -0500

    logger practice

--- NodePractice/index.js
+++ NodePractice/index.js
@@ -4,3 +4,13 @@ const person1 = new Person('John Doe', 2)
 console.log(person1)
 person1.greetings()
 console.log('Hello from Node.js...');
+
+const Logger = require('./logger')
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data))
+
+logger.log('Hello World')
+
+//For homework, output logs to a new file!
