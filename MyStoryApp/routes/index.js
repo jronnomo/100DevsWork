eadc243fad895c3df5bc752d35983958ca5eacdb
jronnomo <ggronnii@gmail.com>
2023-02-08 05:32:16 -0500

    MyStory App using Express, MongoDB, and Google OAuth

---
+++ MyStoryApp/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// @desc Login/Landing page
+// @router GET /
+router.get('/', (req, res) => {
+    res.send('Login')
+})
+
+// @desc Dashboard
+// @router GET /dashboard
+router.get('/dashboard', (req, res) => {
+    res.send('Dashboard')
+})
+
+module.exports = router
