7889dc156b11cd9c22a09fed1774ac3da4b344fd
jronnomo <ggronnii@gmail.com>
2023-03-02 09:30:21 -0500

    created the Public Stories page! Has customization options for different logged in users

--- MyStoryApp/routes/stories.js
+++ MyStoryApp/routes/stories.js
@@ -2,7 +2,7 @@ const { application } = require('express');
 const express = require('express')
 const router = express.Router()
 const partials = require('express-partials')
-const {ensureAuth } = require('../middleware/auth')
+const {ensureAuth} = require('../middleware/auth')
 
 const Story = require('../models/Story')
 
@@ -16,16 +16,34 @@ router.use(partials());
 });
 
 // @desc Process the add form
-// @router GET /stories/
+// @router POST /stories/
 router.post('/', ensureAuth, async (req, res) => {
   try{
-    req.body.user = req.user.id
-    await Story.create(req.body)
-    res.redirect('/dashboard')
+      req.body.user = req.user.id
+      await Story.create(req.body)
+      res.redirect('/dashboard')
   } catch(err){
+      console.log(err)
+      res.render('error/500')
+  }
+});
+
+// @desc Show all stories
+// @router GET /stories/add
+//don't need /stories below here because it's already referenced in our app.js routes
+router.get('/', ensureAuth, async(req, res) => {
+  try{
+    const stories = await Story.find({ status: 'public'})
+      .populate('user')
+      .sort({ createdAt: 'desc'})
+      .lean()
+    res.render('stories/index', {
+      stories,
+    })
+} catch(err){
     console.log(err)
     res.render('error/500')
-  }
+}
 });
 
 module.exports = router
