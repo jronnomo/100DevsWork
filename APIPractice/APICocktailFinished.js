87d5ac7a2d86ae3809f36074ab4dbeb9ef30729c
jronnomo <ggronnii@gmail.com>
2023-03-24 09:46:29 -0400

    few days practice

--- APIPractice/APICocktailFinished.js
+++ APIPractice/APICocktailFinished.js
@@ -17,3 +17,19 @@ function getDrink(){
             console.log(`error is ${err}`)
         });
 }
+
+document.querySelector('button').addEventListener('click', fetchDrink)
+
+async function fetchDrink(){
+    try{
+        let drink = document.querySelector('input').value
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+        const data = await response.json()
+        document.querySelector('#h2').value = data[0].strDrink
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+    }
+    catch(err){
+        console.log(`error is ${err}`)
+    }
+}
